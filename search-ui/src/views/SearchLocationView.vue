b45ec36f438095df79ae3b2ea40674341587ffa9
Isaac Murtagh <isaac.murtagh@centrapay.com>
2022-09-08 17:01:19 +1200

    WIP: google map

--- search-ui/src/views/SearchLocationView.vue
+++ search-ui/src/views/SearchLocationView.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="home">
-    SearchLocationView
+
+  <div>
+    <button @click="getZoom">get zoom</button>
+    <GoogleMap api-key="AIzaSyA39z1GT9YkvGR-z7bFVf4_3bkgi-cbaxM" style="width: 100%; height: 500px" :center="center" :zoom="15">
+      <Marker :options="markerOptions" />
+    </GoogleMap>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
 
-export default {
-  name: 'HomeView',
-  components: {
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: -39.429464, lng: 175.277836 };
+    const markerOptions = { position: center, label: "L", title: "LADY LIBERTY" };
+    return { center, markerOptions };
+  },
+
+  data() {
+    return {
+      markers: []
+    }
+  },
+
+  methods: {
+    getZoom() {
+      const mapObject = new google.maps.Map(document.getElementById("map"));
+      console.log(mapObject.getZoom());
+    }
   }
-}
+});
 </script>
